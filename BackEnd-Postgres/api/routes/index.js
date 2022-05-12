8625db0e5efab3e4dd99dfb8ed0ba4f03bb9fd9b
Renato Albuquerque <84095953+RenatoAlbuquerque@users.noreply.github.com>
2022-05-12 17:26:52 -0300

    update

--- BackEnd-Postgres/api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require("body-parser");
-const pessoas = require("./pessoasRoute");
-
-module.exports = (app) => {
-  app.use(bodyParser.json(), pessoas);
-  app.get("/", (req, res) =>
-    res.status(200).send({ mensagem: "Bem vindo a API" })
-  );
-};
